fb3215e541659361468aaaaccfdab2ae58eace0c
Allan Saddi <allan@saddi.com>
2024-09-09 02:22:53 -0700

    Add custom "LLM Prompt" node, which is just a multi-line string primitive. Add temperature to LLM node.

--- yallmnodes.py
+++ yallmnodes.py
@@ -1,5 +1,31 @@
 import os
+
 import openai
+
+
+class LLMPrompt:
+    def __init__(self):
+        pass
+
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            'required': {
+                'prompt': ('STRING', {
+                    'multiline': True,
+                })
+            },
+        }
+
+    RETURN_TYPES = ('STRING',)
+    RETURN_NAMES = ('prompt',)
+
+    FUNCTION = 'execute'
+
+    CATEGORY = 'YALLM'
+
+    def execute(self, prompt):
+        return (prompt,)
 
 
 class OpenAILikeLLM:
@@ -12,17 +38,20 @@ class OpenAILikeLLM:
     def INPUT_TYPES(cls):
         return {
             'required': {
-                'prompt': ('STRING', {
+                'user_prompt': ('STRING', {
                     'forceInput': True,
                 }),
                 'base_url': ('STRING', {
                     'multiline': False,
                     'default': 'http://localhost:8080/v1',
-                    'lazy': True,
                 }),
                 'model': (['model1', 'model2', 'model3'], {
                     'default': 'model1', # TODO
-                })
+                }),
+                'temperature': ('FLOAT', {
+                    'min': 0.0,
+                    'default': 0.8,
+                }),
             },
             'optional': {
                 'system_prompt': ('STRING', {
@@ -31,7 +60,6 @@ class OpenAILikeLLM:
                 'api_key_env_var': ('STRING', {
                     'multiline': False,
                     'default': '',
-                    'lazy': True,
                 }),
             }
         }
@@ -43,11 +71,11 @@ class OpenAILikeLLM:
 
     CATEGORY = 'YALLM'
 
-    # def check_lazy_status(self, prompt, base_url, model, system_prompt, api_key_env_var):
-    #     # TODO Not yet really clear what this is supposed to do
+    # def check_lazy_status(self, user_prompt, base_url, model, system_prompt, api_key_env_var):
+    #     # TODO don't really have anything lazy yet
     #     return []
 
-    def execute(self, prompt, base_url, model, system_prompt=None, api_key_env_var=None):
+    def execute(self, user_prompt, base_url, model, temperature, system_prompt=None, api_key_env_var=None):
         api_key = "none" # The openai module needs *something*
         if api_key_env_var:
             api_key = os.environ[api_key_env_var] # TODO is there a way to fail gracefully or at least display error message?
@@ -57,12 +85,12 @@ class OpenAILikeLLM:
         messages = []
         if system_prompt:
             messages.append({'role': 'system', 'content': system_prompt})
-        messages.append({'role': 'user', 'content': prompt})
+        messages.append({'role': 'user', 'content': user_prompt})
 
         output = llm.chat.completions.create(
             messages=messages,
             model=model,
-            temperature=0.8, # TODO
+            temperature=temperature,
         )
 
         # TODO Since we're not streaming, we'll need some sort of timeout. How to specify that?
@@ -71,9 +99,11 @@ class OpenAILikeLLM:
 
 
 NODE_CLASS_MAPPINGS = {
+    'LLMPrompt': LLMPrompt,
     'OpenAILikeLLM': OpenAILikeLLM,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
+    'LLMPrompt': 'LLM Prompt',
     'OpenAILikeLLM': 'OpenAI-like LLM',
 }
