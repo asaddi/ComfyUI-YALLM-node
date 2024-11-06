15472f611d39f284a325eb0c58b8918f9c1f8b1a
Allan Saddi <allan@saddi.com>
2024-11-05 22:40:19 -0800

    Lint/reformat JS side.

--- js/yallm-node.js
+++ js/yallm-node.js
@@ -2,72 +2,84 @@ import { app } from "../../scripts/app.js";
 import { api } from "../../scripts/api.js";
 app.registerExtension({
 	name: "YALLM.node",
-    async beforeRegisterNodeDef(nodeType, nodeData, app) {
-        if (nodeType.comfyClass==="LLMProvider") {
-            nodeType.prototype.myRefreshModels = function (node, name) {
-                const provWidget = node.widgets.find((widget) => widget.name === "provider");
-                const modelWidget = node.widgets.find((widget) => widget.name === "model");
-                const fetchWidget = node.widgets.find((widget) => widget.name === "fetch models");
+	async beforeRegisterNodeDef(nodeType, nodeData, app) {
+		if (nodeType.comfyClass === "LLMProvider") {
+			nodeType.prototype.myRefreshModels = (node, name) => {
+				const provWidget = node.widgets.find(
+					(widget) => widget.name === "provider",
+				);
+				const modelWidget = node.widgets.find(
+					(widget) => widget.name === "model",
+				);
+				const fetchWidget = node.widgets.find(
+					(widget) => widget.name === "fetch models",
+				);
 
-                if (!name) name = provWidget.value;
+				const provName = name ?? provWidget.value;
 
-                const myDisableWidgets = (state) => {
-                    provWidget.disabled = state;
-                    modelWidget.disabled = state;
-                    // Note: This is the "fetch models" button we add down below
-                    fetchWidget.disabled = state;
-                };
-                myDisableWidgets(true);
+				const myDisableWidgets = (state) => {
+					provWidget.disabled = state;
+					modelWidget.disabled = state;
+					// Note: This is the "fetch models" button we add down below
+					fetchWidget.disabled = state;
+				};
+				myDisableWidgets(true);
 
-                api.fetchApi(`/llm_models?name=${name}`).then((resp) => {
-                    resp.json().then((data) => {
-                        let widget = modelWidget;
-                        widget.options.values = data;
+				api.fetchApi(`/llm_models?name=${provName}`).then((resp) => {
+					resp.json().then((data) => {
+						const widget = modelWidget;
+						widget.options.values = data;
 
-                        if (!widget.options.values.includes(widget.value) && widget.options.values.length > 0) {
-                            widget.value = widget.options.values[0];
-                        }
+						if (
+							!widget.options.values.includes(widget.value) &&
+							widget.options.values.length > 0
+						) {
+							widget.value = widget.options.values[0];
+						}
 
-                        myDisableWidgets(false);
-                    })
-                })
-            }
-        }
-    },
-    async nodeCreated(node) {
-        if (node?.comfyClass==="LLMProvider") {
-            const provWidget = node.widgets.find((widget) => widget.name === "provider");
-            const modelWidget = node.widgets.find((widget) => widget.name === "model");
+						myDisableWidgets(false);
+					});
+				});
+			};
+		}
+	},
+	async nodeCreated(node) {
+		if (node?.comfyClass === "LLMProvider") {
+			const provWidget = node.widgets.find(
+				(widget) => widget.name === "provider",
+			);
+			const modelWidget = node.widgets.find(
+				(widget) => widget.name === "model",
+			);
 
-            // Whenever provider changes value, fetch models
-            const original_callback = provWidget.callback;
-            provWidget.callback = function () {
-                let name = arguments?.[0];
-                node.myRefreshModels(node, name);
-                return original_callback?.apply(this, arguments);
-            }
+			// Whenever provider changes value, fetch models
+			const original_callback = provWidget.callback;
+			provWidget.callback = function (...args) {
+				const name = args?.[0];
+				node.myRefreshModels(node, name);
+				return original_callback?.apply(this, args);
+			};
 
-            // Disable model combo widget until we fetch models
-            modelWidget.disabled = true;
+			// Disable model combo widget until we fetch models
+			modelWidget.disabled = true;
 
-            // Add button to fetch models manually
-            const btn = node.addWidget(
-                "button",
-                "fetch models",
-                "models",
-                () => { node.myRefreshModels(node) }
-            )
-            btn.serializeValue = () => void 0;
-        }
-    },
-    async setup() {
-        api.addEventListener("executed", function (event) {
-            var node = app.graph.getNodeById(event.detail.node);
-            if (node?.comfyClass==="LLMTextLatch") {
-                const textWidget = node.widgets.find((widget) => widget.name === "text");
-                textWidget.value = event.detail.output.text.join("");
-                app.graph.setDirtyCanvas(true, false);
-            }
-        });
-    }
-})
+			// Add button to fetch models manually
+			const btn = node.addWidget("button", "fetch models", "models", () => {
+				node.myRefreshModels(node);
+			});
+			btn.serializeValue = () => void 0;
+		}
+	},
+	async setup() {
+		api.addEventListener("executed", (event) => {
+			const node = app.graph.getNodeById(event.detail.node);
+			if (node?.comfyClass === "LLMTextLatch") {
+				const textWidget = node.widgets.find(
+					(widget) => widget.name === "text",
+				);
+				textWidget.value = event.detail.output.text.join("");
+				app.graph.setDirtyCanvas(true, false);
+			}
+		});
+	},
+});
